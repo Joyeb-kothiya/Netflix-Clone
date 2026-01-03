280d6ab667deb2a900c620a135e1ec5c78892441
joyeb-kothiya <zikifukato37@gmail.com>
2026-01-03 17:49:21 +0530

    NavBar Fixed

--- script.js
+++ script.js
@@ -95,3 +95,15 @@ function init() {
 }
 
 init();
+
+// NAVBAR SCROLL EFFECT
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (!navbar) return;
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
